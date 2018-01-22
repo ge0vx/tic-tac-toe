7d9a2362eef991bc82ed15984d990a8581e88e89
giovany303 <giovany@303software.com>
2018-01-22 14:52:55 -0600

    determine winner

--- src/App/App.js
+++ src/App/App.js
@@ -17,7 +17,8 @@ class App extends React.Component {
         this.moves = 0;
 
         this.setState({
-            data: {}
+            data: {},
+            currentPlayer: ""
         });
     }
 
@@ -25,7 +26,7 @@ class App extends React.Component {
         if (this.state.data[row_index + '' + col_index]) {
             return;
         }
-        
+
         //number of moves
         this.moves++;
 
@@ -36,9 +37,49 @@ class App extends React.Component {
             data:{
                 ...this.state.data,
                 [row_index + '' + col_index]: playerType
-            }
+            },
+            currentPlayer: playerType
         });
         
+    }
+
+    determineWinner(player) {
+        const grid_size = this.state.grid_size;
+        const currentData = this.state.data;
+        let vertical_count = 0, horizontal_count = 0, right_to_left_count = 0, left_to_right_count = 0;
+
+        for (let i = 0; i < grid_size; i++) {
+            vertical_count = 0;
+            horizontal_count = 0;
+
+            for (let j = 0; j < grid_size; j++) {
+                if (currentData[i + '' + j] == player) {
+                    horizontal_count++;
+                }
+                if (currentData[j + '' + i] == player) {
+                    vertical_count++;
+                }
+            }
+
+            if (currentData[i + '' + i] == player) {
+                left_to_right_count++;
+            }
+
+            if (currentData[(grid_size - 1 - i) + '' + i] == player) {
+                right_to_left_count++;
+            }
+
+            if (horizontal_count == grid_size || vertical_count == grid_size) {
+                return true;
+            }
+
+        }
+
+        if (left_to_right_count == grid_size || right_to_left_count == grid_size) {
+            return true;
+        }
+
+        return false;
     }
 
     render() {
@@ -55,6 +96,16 @@ class App extends React.Component {
 
     componentWillMount() {
         this.init();
+    }
+
+    componentDidUpdate(){
+        console.log(this.state);
+
+        const cPlayer = this.state.currentPlayer;
+        
+        if (this.determineWinner(cPlayer)) {
+            window.alert(cPlayer + " has won");
+        }
     }
 }
 
