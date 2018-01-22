a3433268845bd84c971cc0908d5d5bff53657416
giovany303 <giovany@303software.com>
2018-01-22 01:42:09 -0600

    project setup

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export {App};
